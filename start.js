e5191e93454f1744a5adf167b436be5f80c499ba
Taras Savka <savka.taras90@gmail.com>
2023-07-16 23:01:51 -0400

    updates

--- start.js
+++ start.js
@@ -1,7 +1,19 @@
 import imagemin from 'imagemin';
 import imageminWebp from 'imagemin-webp';
+import fs from "fs";
+import path from "path";
 const filesLocation = 'images/*.{jpg,jpeg,png}';
 const destinationFolder = 'processed/';
+
+fs.readdir(destinationFolder, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(destinationFolder, file), (err) => {
+      if (err) throw err;
+    });
+  }
+});
 
 imagemin([filesLocation], {
 	destination: destinationFolder,
